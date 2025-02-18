dbfb06e79e1ca1b40c76e65eabbe636b81756781
MykhailoDav <davudenko.mukhailo@gmail.com>
2025-02-18 15:21:55 +0200

    added test.Sample

---
+++ samples/Test.SegmentedControl/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace Test.SegmentedControl;
+
+public partial class App : Application
+{
+    public App() => InitializeComponent();
+
+    protected override Window CreateWindow(IActivationState activationState) => new(new MainPage());
+}
---
+++ samples/Test.SegmentedControl/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.SegmentedControl;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
